d722bbf03d490e582645ca8ec043864cae38df7d
dariafedorchenko02 <123147277+dariafedorchenko02@users.noreply.github.com>
2023-01-20 10:55:17 +0300

    Create footer.php

---
+++ temp/footer.php
@@ -0,0 +1,25 @@
+<footer>
+        <div class="container bg-dark text-white">
+         <div class="row">
+             <div class="col-1"></div>
+             <div class="col-4">                    
+                <div class ="copy">
+                <p class = "p1">
+                Copyright <?php echo '&copy2022'; ?>
+                </p>
+            </div></div>  
+             <div class="col-4">
+                <div class="text">
+                    <p>1</p>
+                    <p>21</p>
+                    <a class="phone" href="#"><i class="fa fa-phone"></i>2</a>&nbsp;&nbsp;
+                    <a class="envelope" href="#"><i class="fa fa-envelope"></i>65</a>
+                </div>
+             </div>
+             <div class="col-2"></div>    
+             <div class="col-1"></div>
+         </div>
+     </div>
+    </footer>
+    </body>
+</html>
